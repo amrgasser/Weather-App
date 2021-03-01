500ae9b96222581dd7afe45d19d44feb5db038e4
amrgasser <amr_gasser@hotmail.com>
2021-03-01 22:26:45 +0200

    update1

---
+++ README.md
@@ -0,0 +1,7 @@
+# First Submission of the weatherapp project
+
+## Items that should be functioning correctly
+1. All Project Environment Setup
+2. APIs and Routes
+
+### Dynamic UI to be done after checking if the functionality is ok
---
+++ website/app.js
@@ -0,0 +1,75 @@
+/* Global Variables */
+let zipcode = "0";
+let feelings = "0";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const baseURL = "http://api.openweathermap.org/data/2.5/forecast?id=";
+const apiKey = "&appid=8351f14c945af4642ed5f1e95dc22839";
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById("generate").addEventListener("click", () => {
+  zipcode = document.getElementById("zip").value;
+  feelings = document.getElementById("feelings").value;
+document.getElementById("content").innerHTML=feelings;
+  let data = getData(baseURL, zipcode, apiKey);
+  
+
+
+});
+
+/* Function called by event listener */
+
+
+/* Function to GET Web API Data*/
+const getData = async (baseURL, zipcode, key) => {
+  const res = await fetch(baseURL + zipcode + key);
+  try {
+    const data = await res.json();
+    console.log(data.list[0].dt_text);
+    // document.getElementById("date").innerHTML=data.list[0].clouds.dt_text;
+
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+/* Function to POST data */
+const postData = async (url = "", data = {}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Body data type must match "Content-Type" header
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+/* Function to GET Project Data */
+const getProjectData = async (url = "") => {
+  
+  const response = await fetch(url)
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
